6850c669be00e7945d4780c1b6570279725edd9c
chnpat <charnon.top@hotmail.com>
2022-09-06 11:53:39 +0900

    Update the portfolio website
    - feat: add modals for citing the papers.

--- index.js
+++ index.js
@@ -8,3 +8,20 @@ form.addEventListener('submit', function handleSubmit(event) {
   // 👇️ Reset form here
   form.reset();
 });
+
+function copyText(name) {
+    /* Get the text field */
+    var copyText = document.getElementById(name);
+  
+    /* Select the text field */
+    copyText.select(); 
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+  
+     /* Copy the text inside the text field */
+    navigator.clipboard.writeText(copyText.value);
+  
+    var myAlert = document.getElementById('copied_alert')
+    myAlert.style.display = 'block';
+    /* Alert the copied text */
+    // alert("Copied the text!");
+  }
